a0282dc17a658ad6cbf8e7a54aa29afc6b6e9d88
PratikChitruk <pratik.chitruk@gmail.com>
2022-02-21 16:43:37 +0530

    calculatedailywage.cs of employeewage

---
+++ CalculateDailyWage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class CalculateDailyWage
+    {
+        public static void CalculateWage() 
+        {
+            int WagePerHour = 20;
+            int FullWorkingDay = 8;
+            int calculateWage = 0;
+
+            Random random = new Random();
+            int number = random.Next(2);
+
+            if (number == 1)
+            {
+                calculateWage  = WagePerHour * FullWorkingDay;
+                
+                Console.WriteLine("calculate Wage = " + calculateWage);
+            }
+            else
+            {
+                calculateWage = 0;
+                Console.WriteLine("calculate Wage = " + calculateWage);
+            }
+        }
+    }
+}
